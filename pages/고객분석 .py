2a396a222e55a222bca188e3620e69960e5b6690
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-03 11:10:32 +0900

    Update 고객분석 .py

--- pages/고객분석 .py
+++ pages/고객분석 .py
@@ -7,7 +7,14 @@ st.header('고객분석')
 with st.sidebar:
   choose = option_menu("고객분석",["part 1. 나이별 분석","part 2. 지역별 분석","part 3. 유저 분석 "],
                        icons=['house', 'camera fill', 'kanban'],
-                       menu_icon="app-indicator", default_index=0)
+                       menu_icon="app-indicator", default_index=0,
+                       styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
 st.write("""
 ### DATA 전처리
 - 0세 => 15세
