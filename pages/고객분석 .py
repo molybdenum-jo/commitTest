c5fe04cae23a638b68e4e849330f843e8ca8e315
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-03 11:17:11 +0900

    Update 고객분석 .py

--- pages/고객분석 .py
+++ pages/고객분석 .py
@@ -4,17 +4,8 @@ import numpy as np
 
 
 st.header('고객분석')
-with st.sidebar:
-    choice = option_menu("Menu", ["페이지1", "페이지2", "페이지3"],
-                         icons=['house', 'kanban', 'bi bi-robot'],
-                         menu_icon="app-indicator", default_index=0,
-                         styles={
-        "container": {"padding": "4!important", "background-color": "#fafafa"},
-        "icon": {"color": "black", "font-size": "25px"},
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#fafafa"},
-        "nav-link-selected": {"background-color": "#08c7b4"},
-    }
-    )
+st.sidebar.title('고객분석')
+
 st.write("""
 ### DATA 전처리
 - 0세 => 15세
