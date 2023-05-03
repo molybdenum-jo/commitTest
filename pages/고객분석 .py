72d3ccc6fc0979cc270de85ac1ca62f026277c2e
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-03 16:10:55 +0900

    Update 고객분석 .py

--- pages/고객분석 .py
+++ pages/고객분석 .py
@@ -77,15 +77,17 @@ import seaborn as sns
 
 df = pd.read_csv('data/Country.csv')
 
-countriestop10=df['Country'].value_counts().reset_index().head(10)
-countriestop10 = countriestop10.rename(columns={'index':'Country'})
+countriestop10 = df['Country'].value_counts().reset_index().head(10)
+countriestop10.columns = ['Country', 'Count']
 countriestop10.set_index('Country', inplace=True)
 
-fig3, ax = sns.barplot(data=df, x=countriestop10.index.values, y=countriestop10.Country)
-ax.set_title('Number of ratings by country') # 그래프 제목 설정
+countriestop10pct = df['Country'].value_counts(normalize=True).reset_index().head(10)
+countriestop10pct.columns = ['Country', 'Percentage']
+countriestop10pct.set_index('Country', inplace=True)
 
-# 그래프 표시하기
-st.pyplot(fig3)
+fig3, ax = plt.subplots(figsize=(10, 6))
+sns.barplot(data=countriestop10, x=countriestop10.index, y='Count', ax=ax)
+ax.set_title('Number of ratings by country')
 
 
 js = "window.scrollTo(0, document.getElementById('part-3-user-analysis').offsetTop);"
