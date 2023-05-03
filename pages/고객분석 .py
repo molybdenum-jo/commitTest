365cbe0ede5ced4eafbb6d7507b9c00359efb718
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-03 10:47:57 +0900

    Update and rename Part1.연령별 평점 비교 .py to 고객분석 .py

--- pages/고객분석 .py
+++ pages/고객분석 .py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 
 
-st.header('Part1. 연령별 평점 비교')
-st.sidebar.title('고객분석')
+st.header('고객')
+st.sidebar.title('Part1. 연령별 평점 비교')
 st.write("""
 ### DATA 전처리
 - 0세 => 15세
