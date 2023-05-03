a03e922968bd94ac2dcbc0f4a978beda43ad7402
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-03 13:26:04 +0900

    Update 고객분석 .py

--- pages/고객분석 .py
+++ pages/고객분석 .py
@@ -23,14 +23,23 @@ st.dataframe(df_train)
 st.write(""" 
 ### part 1. 나이별 분석
 """)
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 데이터 불러오기
 df = pd.read_csv('data/AGE.csv')
 
+# 히스토그램 그리기
 fig4 = plt.figure()
-plt.hist(data= df, x = 'Age',bins=30,rwidth=0.8)
+plt.hist(data=df, x='Age', bins=30, rwidth=0.8)
 plt.title('Age Distribution') # 그래프 제목 설정
 plt.xlabel('Age') # x축 레이블 설정
 plt.ylabel('Count') # y축 레이블 설정
+
+# 그래프 표시하기
 st.pyplot(fig4)
+
 
 
 
