6052ace724b97900ea6a496a07c92afd8a9b69bd
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-03 11:12:05 +0900

    Update 고객분석 .py

--- pages/고객분석 .py
+++ pages/고객분석 .py
@@ -5,10 +5,10 @@ import numpy as np
 
 st.header('고객분석')
 with st.sidebar:
-  choose = option_menu("고객분석",["part 1. 나이별 분석","part 2. 지역별 분석","part 3. 유저 분석 "],
-                       icons=['house', 'camera fill', 'kanban'],
-                       menu_icon="app-indicator", default_index=0,
-                       styles={
+    choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning"],
+                         icons=['house', 'camera fill', 'kanban'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
         "container": {"padding": "5!important", "background-color": "#fafafa"},
         "icon": {"color": "orange", "font-size": "25px"}, 
         "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
