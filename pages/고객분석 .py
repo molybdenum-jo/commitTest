4bfe6e9eaa62c2fd57f33be147700ee0fe453c37
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-03 11:36:34 +0900

    Update 고객분석 .py

--- pages/고객분석 .py
+++ pages/고객분석 .py
@@ -8,8 +8,31 @@ st.sidebar.title('고객분석')
 
 st.write("""
 ### DATA 전처리
-- 0세 => 15세
-- 100세 이상 => 67세
+- 0세 => 15세(상위1%)
+- 80세 이상(하위4%이상) => 80세
+""")
 
+st.write("""
 ### Dataset
+""")
+df_train = pd.read_csv('data/AGE.csv')
+st.dataframe(df_train)
+
+st.write(""" 
+### part 1. 나이별 분석
+""")
+import streamlit as st
+from PIL import Image
+
+image = Image.open('image.jpg')
+st.image(image, caption='Image Caption')
+
+
+st.write(""" 
+### part 2. 지역별 분석
+""")
+
+
+st.write(""" 
+### part 3. 유저 분석 
 """)
