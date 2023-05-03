cddb6ad475c6bf89ee1d2f010bd371b8b992d4ec
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-03 11:10:09 +0900

    Update 고객분석 .py

--- pages/고객분석 .py
+++ pages/고객분석 .py
@@ -3,8 +3,11 @@ import pandas as pd
 import numpy as np
 
 
-st.header('고객')
-st.sidebar.title('Part1. 연령별 평점 비교')
+st.header('고객분석')
+with st.sidebar:
+  choose = option_menu("고객분석",["part 1. 나이별 분석","part 2. 지역별 분석","part 3. 유저 분석 "],
+                       icons=['house', 'camera fill', 'kanban'],
+                       menu_icon="app-indicator", default_index=0)
 st.write("""
 ### DATA 전처리
 - 0세 => 15세
