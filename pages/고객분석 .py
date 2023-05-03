4c4c4b6c05546f3546c8b388acb81c4f1bc18001
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-03 14:08:20 +0900

    Update 고객분석 .py

--- pages/고객분석 .py
+++ pages/고객분석 .py
@@ -9,12 +9,10 @@ st.header('고객분석')
 st.sidebar.title('고객분석')
 st.sidebar.markdown("""
     ## Table of Contents
-    - [part 1. 나이별 분석]
-    - [part 2. 지역별 분석]
-    - [part 3. 유저별 분석]
+    - [part 1. 나이별 분석](#part-1-age-analysis)
+    - [part 2. 지역별 분석](#part-2-location-analysis)
+    - [part 3. 유저별 분석](#part-3-user-analysis)
 """)
-st.sidebar.subheader("part 2. 지역별 분석")
-st.sidebar.subheader("part 3. 유저 분석 ")
 
 st.write("""
 ### DATA 전처리
@@ -28,15 +26,31 @@ st.write("""
 df_train = pd.read_csv('data/AGE.csv')
 st.dataframe(df_train)
 
-# part 1
-st.write(""" 
-### part 1. 나이별 분석
-""")
-st.write('<div id="part-1"></div>', unsafe_allow_html=True)
+# Part 1. 나이별 분석
+st.markdown("<h1 id='part-1-age-analysis'>Part 1. 나이별 분석</h1>", unsafe_allow_html=True)
 
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
+# Part 2. 지역별 분석
+st.markdown("<h1 id='part-2-location-analysis'>Part 2. 지역별 분석s</h1>", unsafe_allow_html=True)
+
+# Part 3. 유저별 분석
+st.markdown("<h1 id='part-3-user-analysis'>Part 3. 유저별 분석</h1>", unsafe_allow_html=True)
+
+# 소제목 클릭 시 해당 위치로 이동
+if st.sidebar.button("Part 1. 나이별 분석"):
+    js = "window.scrollTo(0, document.getElementById('part-1-age-analysis').offsetTop);"
+    html = '<script>{}</script>'.format(js)
+    st.markdown(html, unsafe_allow_html=True)
+
+if st.sidebar.button("Part 2. 지역별 분석"):
+    js = "window.scrollTo(0, document.getElementById('part-2-location-analysis').offsetTop);"
+    html = '<script>{}</script>'.format(js)
+    st.markdown(html, unsafe_allow_html=True)
+    
+if st.sidebar.button("Part 3. 유저별 분석"):
+    js = "window.scrollTo(0, document.getElementById('part-3-user-analysis').offsetTop);"
+    html = '<script>{}</script>'.format(js)
+    st.markdown(html, unsafe_allow_html=True)
+    
 
 # 데이터 불러오기
 df = pd.read_csv('data/AGE.csv')
